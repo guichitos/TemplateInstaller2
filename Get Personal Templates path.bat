8aea59704232b34c4478e7070a6b3957ec4e0502
guichitos <92882295+guichitos@users.noreply.github.com>
2025-10-31 16:12:01 -0400

    Add script to query PersonalTemplates registry value

---
+++ Get Personal Templates path.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal EnableDelayedExpansion
+set "KEY=HKCU\Software\Microsoft\Office\16.0\PowerPoint\Options"
+set "VALUE=PersonalTemplates"
+set "Data="
+
+for /f "skip=2 tokens=1,2,*" %%A in ('reg query "%KEY%" /v "%VALUE%" 2^>nul') do (
+    set "Data=%%C"
+)
+
+if defined Data (
+    echo %VALUE% = !Data!
+) else (
+    echo No se pudo encontrar el valor "%VALUE%" en "%KEY%".
+    exit /b 1
+)
+
+endlocal
